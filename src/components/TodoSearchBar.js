9da626c7d90964c2d6b490bf2bd3768bd4d76213
Jeison <jei_son1516@hotmail.com>
2023-11-17 08:38:05 -0500

    feat(TodoSearchBar): implement search functionality in component
    - Introduce state for searchValue to manage input value
    - Bind input value to the state for controlled input behavior
    - Add event handler to update searchValue on input changes
    - Improve input element with additional attributes for accessibility

--- src/components/TodoSearchBar.js
+++ src/components/TodoSearchBar.js
@@ -1,11 +1,26 @@
+import React from 'react'
 import '../styles/todoSearchBar.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 function TodoSearchBar() {
+  const [searchValue, setSearchValue] = React.useState('');
   return (
     <div className='input-container'>
-      <input type="text" placeholder="Search..." />
+      <input 
+        type="text" 
+        placeholder="Search..."
+        value={searchValue}
+        className="search-input"
+        name="search"
+        id="search"
+
+        onChange={
+          (event) => {
+            setSearchValue(event.target.value)
+          }
+        }
+      />
       <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon"/>
     </div>
   )
