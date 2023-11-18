dcb46881b4375f942361e3f1e9637e99a0cb0558
Jeison <jei_son1516@hotmail.com>
2023-11-18 07:51:14 -0500

    feat(TodoItem): implement onClick callbacks for complete and delete actions
    - Introduce onComplete and onDelete callbacks in TodoItem
    - Update TodoItem component to trigger these callbacks on user interactions

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,24 @@
-import '../styles/todoItem.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-regular-svg-icons'
-import { faCircleCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
+import "../styles/todoItem.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faCircleCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-function TodoItem({ text, completed }) {
+function TodoItem(props) {
   return (
-    <li className={completed? 'completed': null}>
+    <li className={props.completed ? "completed" : null}>
       <span>
-        <FontAwesomeIcon icon={completed? faCircleCheck: faCircle} size="lg" />
+        <FontAwesomeIcon
+          icon={props.completed ? faCircleCheck : faCircle}
+          size="lg"
+          onClick={props.onComplete}
+        />
       </span>
-      <p>{text}</p>
+      <p>{props.text}</p>
       <span>
-        <FontAwesomeIcon icon={faXmark} size="2xl"/>
+        <FontAwesomeIcon icon={faXmark} size="2xl" onClick={props.onDelete} />
       </span>
     </li>
-  )
+  );
 }
 
-export { TodoItem }
+export { TodoItem };
