8453d097edf3406e80c5aab9df0e83bcfe3f7d51
Jeison <jei_son1516@hotmail.com>
2023-11-20 13:39:20 -0500

    chore(App): update placeholder text in defaultTodos for layout testing
    - Replace the existing todo text with a longer Lorem Ipsum paragraph
    - This change is solely for testing and evaluating the layout visualization

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./styles/App.scss";
 import React from "react";
 
 const defaultTodos = [
-  { text: "Learn HTML", completed: true },
+  { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", completed: true },
   { text: "Learn React", completed: false },
   { text: "Learn Redux", completed: false },
   { text: "Learn React Router", completed: false },
