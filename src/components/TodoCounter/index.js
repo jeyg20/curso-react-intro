9886252035bf302164bfee4e2b2e960820affce2
Jeison Guerrero <jei_son1516@hotmail.com>
2023-11-25 21:04:58 -0500

    style: Add loading and empty states to TodoCounter
      - Updated TodoCounter component to handle loading and empty states

--- src/components/TodoCounter/index.js
+++ src/components/TodoCounter/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./TodoCounter.scss";
 
-function TodoCounter({ total, completed }) {
+function TodoCounter({ total, completed, isLoading }) {
   return (
     <div className="todo-counter">
-      {completed === total ? (
+      {isLoading && <h1 className="loading-todos">Loading todos...</h1>}
+      {total === 0 && !isLoading && (
+        <h1>You don't have todos, create your first one</h1>
+      )}
+      {total > 0 && completed === total && (
         <>
           <h1>You have completed all your {total} todos</h1>
           <span>🎉</span>
         </>
-      ) : (
+      )}
+      {total > 0 && completed !== total && (
         <h1>
           You have completed {completed} of {total} todos
         </h1>
