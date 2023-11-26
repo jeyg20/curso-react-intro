d1e28c9db4b215ab1ef88538d96e2ba5ad3f8b6d
Jeison Guerrero <jei_son1516@hotmail.com>
2023-11-26 14:34:52 -0500

    Fix: Correct useLocalStorage import path in TodoContext

--- src/components/TodoContext/index.js
+++ src/components/TodoContext/index.js
@@ -1,5 +1,5 @@
 import React, { createContext } from "react";
-import { useLocalStorage } from "../../App/useLocalStorage";
+import { useLocalStorage } from "./useLocalStorage";
 
 const TodoContext = createContext();
 
