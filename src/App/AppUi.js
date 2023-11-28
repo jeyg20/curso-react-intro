8b0ffa67e10d2215b56358f05bbf37487a8fd714
Jeison Guerrero <jei_son1516@hotmail.com>
2023-11-28 13:38:33 -0500

    feat: Add modal and form components to AppUi

--- src/App/AppUi.js
+++ src/App/AppUi.js
@@ -7,31 +7,44 @@ import { TodosLoading } from "../components/TodosLoading";
 import { TodosError } from "../components/TodosError";
 import { TodosEmpty } from "../components/TodosEmpty";
 import { TodoContext } from "../components/TodoContext";
+import { useContext } from "react";
+import { Modal } from "../components/Modal";
+import { TodoForm } from "../components/TodoForm";
 
 function AppUi() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = useContext(TodoContext);
   return (
     <>
       <TodoCounter />
       <TodoSearchBar />
-      <TodoContext.Consumer>
-        {({ loading, error, searchedTodos, completeTodo, deleteTodo }) => (
-          <TodoList>
-            {loading && <TodosLoading />}
-            {error && <TodosError />}
-            {!loading && !searchedTodos.length && <TodosEmpty />}
-            {searchedTodos.map((todo) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-              />
-            ))}
-          </TodoList>
-        )}
-      </TodoContext.Consumer>
-      <TodoCreateButton />
+      <TodoList>
+        {loading && <TodosLoading />}
+        {error && <TodosError />}
+        {!loading && !searchedTodos.length && <TodosEmpty />}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+      <TodoCreateButton setOpenModal={setOpenModal} />
+      {openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
     </>
   );
 }
