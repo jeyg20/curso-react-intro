b67d8ba7e0955a7c5aa08ef820738022b15de445
Jeison Guerrero <jei_son1516@hotmail.com>
2023-12-04 10:24:18 -0500

    refactor(TodoCreateButton): update prop name for modal control
    - Rename setOpenModal prop to setOpenCreateTodo to better reflect its purpose.
    - Adjust the component logic to use the updated prop name.

--- src/components/TodoCreateButton/index.js
+++ src/components/TodoCreateButton/index.js
@@ -2,13 +2,13 @@ import "./TodoCreateButton.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
-function TodoCreateButton({ setOpenModal }) {
+function TodoCreateButton({ setOpenCreateTodo }) {
   return (
     <div className="add-btn-container">
       <button
         className="add-btn"
         onClick={() => {
-          setOpenModal((state) => !state);
+          setOpenCreateTodo((state) => !state);
         }}
       >
         <FontAwesomeIcon icon={faCirclePlus} className="add-btn-icon" />
